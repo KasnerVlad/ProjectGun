07e3f38c58fc762e5d41004acd4f0d8fc8172667
KasnerVlad <kasner.vladislav@gmail.com>
2025-03-12 18:09:15 +0200

    Add SaveSystem to project(but I am not sure that it without bugs) and sort groups fiels by folders

---
+++ Assets/StarterAssets/FirstPersonController/Scripts/SaveSystem/JsonSaveSystem.cs
@@ -0,0 +1,40 @@
+using System;
+using System.IO;
+using UnityEngine;
+public class JsonSaveSystem : ISaveSystem
+{
+    private readonly string _filePath;
+
+    public JsonSaveSystem()
+    {
+        _filePath = Application.persistentDataPath + "/save.json";
+        Debug.Log(_filePath);
+    }
+
+    public void Save<T>(T saveData)
+    {
+        var json = JsonUtility.ToJson(saveData);
+        using (var writer = new StreamWriter(_filePath))
+        {
+            writer.Write(json);
+        }
+    }
+    public T Load<T>()
+    {
+        string json = "";
+        using (var reader = new StreamReader(_filePath))
+        {
+            string line;
+            while ((line = reader.ReadLine())!=null)
+            {
+                json += line;
+            }
+        }
+        if (string.IsNullOrEmpty(json))
+        {
+            return Activator.CreateInstance<T>();
+        }
+        Debug.Log(json);
+        return JsonUtility.FromJson<T>(json);
+    }
+}
---
+++ Assets/StarterAssets/FirstPersonController/Scripts/SaveSystem/SaveData.cs
@@ -0,0 +1,11 @@
+using System;
+using UnityEngine;
+using StarterAssets.FirstPersonController.Scripts.SOLIDInventory;
+[Serializable]
+public class GameData
+{
+    public Vector3 PlayerPosition;/*
+    public InventorySlots[] PlayerSlots;*/
+    public string SaveName = "";
+    public Quaternion PlayerRotation;
+}
